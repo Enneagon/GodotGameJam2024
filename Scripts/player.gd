8195abdb6b7b462ac4eb9f1f3a79e5b54146afd4
Enneagon <alexrjennings@gmail.com>
2024-05-25 17:07:26 -0400

    Accel-Free movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -3,15 +3,14 @@ extends CharacterBody2D
 # Maybe use a List instead of an array since we're adding and removing elements frequently
 var biteArray = []
 
-func _process(_delta):
-	print(biteArray)
-
 func _physics_process(delta):
 	var direction: Vector2 = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	
-	# Also, do you need to multiply the speed here by delta to make it frame rate independant?
-	velocity.x = move_toward(velocity.x, GlobalVars.playerSpeed * direction.x, GlobalVars.playerAccel)
-	velocity.y = move_toward(velocity.y, GlobalVars.playerSpeed * direction.y, GlobalVars.playerAccel)
+
+	velocity = direction.normalized()
+	position += velocity * GlobalVars.playerSpeed * delta
+	#Old Movement
+	#velocity.x = move_toward(velocity.x, GlobalVars.playerSpeed * direction.x, GlobalVars.playerAccel * delta)
+	#velocity.y = move_toward(velocity.y, GlobalVars.playerSpeed * direction.y, GlobalVars.playerAccel * delta)
 	
 	move_and_slide()
 
