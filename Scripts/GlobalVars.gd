a1cefecb88ff96092f0dac0dc6c1b605b874da8f
Jamie Sullivan-Phillips <47346975+jsullivanphillips@users.noreply.github.com>
2024-05-26 14:27:29 -0700

    added sprinting

--- Scripts/GlobalVars.gd
+++ Scripts/GlobalVars.gd
@@ -13,8 +13,11 @@ const DAMAGE_REDUCTION_MULTIPLIER = 0.5
 const DAMAGE_INCREASE_MULTIPLIER = 1.5
 
 #Player Variables
+var playerSprintSpeedMultiplier = 1.6
+var playerSprintEnergy = 20.0
+var playerSprintEnergyMax = 20.0
 var playerSize = size.SMALL
-var playerSpeed = 100.0
+var playerSpeed = 60.0
 var playerStrength = 1.0
 var playerAttackRange = 1.0
 var playerAttackSpeed = 1.0
@@ -26,7 +29,7 @@ var hungerPointsMax = 25
 var killedBy = "Nothing"
 
 #Continue Variables: These are saved when a player clears a level, and restored when pressing Continue.
-var playerSpeedContinue = 100.0
+var playerSpeedContinue = 60.0
 var playerStrengthContinue = 1.0
 var playerAttackRangeContinue = 1.0
 var playerAttackSpeedContinue = 1.0
@@ -35,7 +38,7 @@ var evoPointsContinue = 0
 var hungerPointsMaxContinue = 25
 
 #Reset Variables: These are the original variables, which the active variables are reset to at the game start.
-var playerSpeedReset = 100.0
+var playerSpeedReset = 60.0
 var playerStrengthReset = 1.0
 var playerAttackRangeReset = 1.0
 var playerAttackSpeedReset = 1.0
