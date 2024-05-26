a72a02893b3bcd366e5e5934573712e2bded3fc4
Jamie Sullivan-Phillips <47346975+jsullivanphillips@users.noreply.github.com>
2024-05-26 14:43:55 -0700

    added dinosaur sprite and background music

--- Scripts/hurtbox.gd
+++ Scripts/hurtbox.gd
@@ -1,7 +1,12 @@
 extends Area2D
 
 var biteRange = 15.0
+@onready var animated_sprite = $"../AnimatedSprite2D"
 
 func _process(_delta):
 	var direction = (get_global_mouse_position() - self.get_parent().position).normalized()
 	position = biteRange * direction
+	if(direction.x < 0):
+		animated_sprite.flip_h = false
+	else:
+		animated_sprite.flip_h = true
